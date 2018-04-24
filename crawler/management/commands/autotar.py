5c8712423a6fbc26ae464ffe36bf16f2768f218e
david <davidtnfsh@gmail.com>
2018-04-24 19:57:02 +0800

    [Bug fix] also check extension and whether it's color photo or not, if not, delete it.

--- crawler/management/commands/autotar.py
+++ crawler/management/commands/autotar.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 import subprocess, os, tqdm, pathlib, shutil
+from PIL import Image
 
 class Command(BaseCommand):
 	help = 'use this cmd tar all folder we crawled from web !!!'
@@ -7,6 +8,24 @@ class Command(BaseCommand):
 
 	def add_arguments(self, parser):
 	    parser.add_argument('pic_type', type=str)
+
+	@staticmethod
+	def check_extension(folder):
+		currentDirectory = pathlib.Path(folder)
+		for currentFile in currentDirectory.iterdir():
+			try:
+				currentFile = str(currentFile)
+				img = Image.open(currentFile)
+				if len(np.array(img).shape) != 3:
+					print('{} is not a colorful pic, remove it'.format(currentFile))
+					os.remove(currentFile)
+					continue
+				filename_extension = '.' + img.format
+				correct_name = currentFile.split('.')[0]+filename_extension
+				os.rename(currentFile, correct_name)
+			except Exception as e:
+				os.remove(currentFile)
+				print('{} rm {}'.format(e, currentFile))
 
 	def handle(self, *args, **options):
 		tar_name = options['pic_type']+'.tar.gz'
@@ -20,5 +39,6 @@ class Command(BaseCommand):
 				for file_name in file_names:
 					subprocess.call(['cp', os.path.join(dir_path, file_name), options['pic_type']])
 
+		self.check_extension(options['pic_type'])
 		subprocess.call(['tar', 'zcvf', tar_name, options['pic_type']])
 		self.stdout.write(self.style.SUCCESS('compress {} finish !!!'.format(tar_name)))
